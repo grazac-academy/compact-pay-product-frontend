231ed6e6d769dc341b00472b056bf4880de934f3
Mayowa Dabiri <38558129+mayowadabiri@users.noreply.github.com>
2022-04-25 13:59:27 +0100

    project setup (#1)

---
+++ src/components/card.js
@@ -0,0 +1,8 @@
+const card = ({title, content }) => {
+    return (
+        <h5>{title}</h5>
+        <p>{content}</p>
+    )
+}
+
+export default card
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/styled-components';
+
+export const Container = styled.div`
+  background-color: #fafafa;
+  display: flex;
+  sassasabnas snbqwbjqw h5 {
+    font-width: 700;
+  }
+
+  p {
+    color: red;
+    font-size: 10px;
+  }
+`;
+export const Wrapper = styled.img`
+  background-color: #fafafa;
+  display: flex;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+export const Button = styled.button`
+  background-color: #fafafa;
+  display: flex;
+`;

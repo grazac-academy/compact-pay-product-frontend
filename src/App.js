49f910b3155ff858b0adfed9711b40be0bd9ab16
Effiong Godwin <gordwynho@gmail.com>
2022-04-29 19:46:21 +0300

    carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 
+
+import Home from './pages/Home/Home'
+
 function App() {
-  return <div className="App">COMPACT PAY PRODUCT</div>;
+  return(
+  <div className="App">
+    <Home/>
+  </div>
+  )
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import  classes from "./Home.module.css";
+import Logo from "../../assets/Landing/world.png";
+import Carousel from "../../components/carousel/Carousel";
+
+
+
+
+
+function Home() {
+  return (
+    <div className={classes.full5}>
+     <div className={classes.container}>
+             <h4>Millions of international transactions around the globe </h4>
+       <div className={classes.section5}>
+              <div className={classes.section5a}> 
+               <div className={classes.section5a1}>
+                    <div className={classes.section5a3}>
+                    <div className={classes.section5a2}></div>
+                      <h4>150+</h4>
+                      <p>We operates in over 150+ countries and regions</p>
+                    </div>
+
+                    <div className={classes.section5a31}>
+                    <div className={classes.section5a21}></div>
+                      <h4>2M+</h4>
+                      <p>We have over 2M+ trusted users worldwide</p>
+                    </div>
+
+                    <div className={classes.section5a32}>
+                    <div className={classes.section5a22}></div>
+                      <h4>50</h4>
+                      <p>We complete over 50 transactions per seconds</p>
+                    </div>
+                 </div>    
+              </div>
+           <div className={classes.world}> 
+              <img src={Logo} alt="world"></img>
+            </div>
+        </div>
+     </div>
+      <Carousel/> 
+      
+    </div>
+  )
+}
+
+export default Home;

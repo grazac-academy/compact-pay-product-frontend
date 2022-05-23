9831d122601058cd14bb73773cdaf85f058ed780
Mayowa dabiri <mayowad43@gmail.com>
2022-05-23 10:38:11 +0100

    bug fixzes

--- src/components/CurrencyRow/CurrencyRow.js
+++ src/components/CurrencyRow/CurrencyRow.js
@@ -1,27 +1,26 @@
 import classes from './CurrencyRow.module.css';
 
 const CurrencyRow = (props) => {
-
-    const{
-        currencyOptions,
-        selectCurrency,
-        onChangeCurrency,
-        amount,
-        onChangeAmount,
-    } = props;
-
+  const {
+    currencyOptions,
+    selectCurrency,
+    onChangeCurrency,
+    amount,
+    onChangeAmount,
+  } = props;
 
   return (
     <div className={classes.CurrencyRow}>
-        
-        <input  type="number" value={amount} onChange={onChangeAmount}/>
-        <select value={selectCurrency} onChange={onChangeCurrency}>
-            {currencyOptions.map(option => (
-                 <option key={option} value={option}>{option}</option>
-            ))}
-        </select>
+      <input type="number" value={amount} onChange={onChangeAmount} />
+      <select value={selectCurrency} onChange={onChangeCurrency}>
+        {currencyOptions?.map((option, index) => (
+          <option key={index} value={option.key}>
+            {option.key}
+          </option>
+        ))}
+      </select>
     </div>
-  )
-}
+  );
+};
 
-export default CurrencyRow
+export default CurrencyRow;

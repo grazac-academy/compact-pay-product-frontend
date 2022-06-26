924a1137c6943df32afe57779d588609ebd0af0e
aladechristopher <aladechristoph@gmail.com>
2022-06-26 16:10:54 +0100

    commit "nne"

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,38 +1,8 @@
-<<<<<<< HEAD
 import { baseUrl } from '../axios/base';
 
 
 export const createAccount = async (payload) => {
   const response = baseUrl.post('/register', payload);
-=======
-import axios from "axios";
-import { baseLink } from "../axios/base";
-
-export const createAccount = async (data) => {
-  const response = await axios({
-    method: "post",
-    url: `${baseLink}/auth/register`,
-    data: data,
-  });
-  return response;
-};
-
-export const login = async (data) => {
-  const response = await axios({
-    method: "post",
-    url: `${baseLink}/auth/login`,
-    data: data,
-  });
-  return response;
-};
-
-export const verify = async (id, otp) => {
-  const response = await axios({
-    method: "post",
-    url: `${baseLink}/auth/verify-email?_id=${id}`,
-    data: { otp: otp },
-  });
->>>>>>> main
   return response;
 };
 

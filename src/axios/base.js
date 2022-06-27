5ed32d278decfd50c11ec84cd6616ecf74a09097
Mayowa dabiri <mayowad43@gmail.com>
2022-06-27 10:59:14 +0100

    bug fixes

--- src/axios/base.js
+++ src/axios/base.js
@@ -1,19 +1,13 @@
 import axios from 'axios';
 
-// export const baseLink =
-//   process.env.NODE_ENV === "development"
-//     ? "https://compact-pay-product.herokuapp.com/api/v1"
-//     : "https://compact-pay-product.herokuapp.com";
-
-    
-export const url = "https://compact-pay-product.herokuapp.com/api/v1/auth";
+export const url = 'https://compact-pay-product.herokuapp.com/api/v1';
 
 export const baseUrl = axios.create({
-  baseUrl: `${url}`,
+  baseUrl: `${url}/auth`,
 });
 
-
 baseUrl.interceptors.request.use((request) => {
-  request.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  const token = localStorage.getItem('token');
+  request.headers.Authorization = `Bearer ${token ? token : ''}`;
   return request;
 });

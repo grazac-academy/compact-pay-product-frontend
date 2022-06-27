2ec93b0f248390a572c0dd7fb7ed53d78415a37b
Mayowa dabiri <mayowad43@gmail.com>
2022-06-27 12:36:52 +0100

    fix verify email endpoint

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,14 +1,20 @@
 import { baseUrl } from '../axios/base';
 
-
 export const createAccount = async (payload) => {
   const response = baseUrl.post('/register', payload);
   return response;
 };
 
-
 export const verify = async (otp, email) => {
-  const response = baseUrl.post('/verify-email', otp, email);
+  const response = baseUrl.post(
+    '/verify-email',
+    { otp },
+    {
+      headers: {
+        email: email,
+      },
+    }
+  );
   return response;
 };
 
@@ -16,5 +22,3 @@ export const login = async (data) => {
   const response = baseUrl.post('/login', data);
   return response;
 };
-
-
